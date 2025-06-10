d2e24fa9b7fef2dc728c932898ac81c653c08e71
kumardurge <kumardurge20@gmail.com>
2025-06-09 22:09:42 -0400

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,10 +1,15 @@
 USE master;
 GO
 
--- Drop and recreate the 'DataWarehouse' database
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+--Drop and recreate the 'DataWarehouse' database
+
+IF EXISTS(SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
     ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE DataWarehouse;
+DROP DATABASE DataWarehouse;
 END;
+GO 
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
 GO
